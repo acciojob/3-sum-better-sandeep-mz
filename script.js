bbb32589b1c9e486fc213a42197268988aec40e1
Sandeep Maurya <108665091+sandeep-mz@users.noreply.github.com>
2023-09-16 19:31:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 function threeSum(arr, target) {
-// write your code here
-	if (nums.length < 3) {
+  if (arr.length < 3) {
     return null; // Not enough elements to form a triplet.
   }
 
-  let closestSum = nums[0] + nums[1] + nums[2];
+  let closestSum = arr[0] + arr[1] + arr[2];
 
-  for (let i = 0; i < nums.length - 2; i++) {
-    for (let j = i + 1; j < nums.length - 1; j++) {
-      for (let k = j + 1; k < nums.length; k++) {
-        const currentSum = nums[i] + nums[j] + nums[k];
+  for (let i = 0; i < arr.length - 2; i++) {
+    for (let j = i + 1; j < arr.length - 1; j++) {
+      for (let k = j + 1; k < arr.length; k++) {
+        const currentSum = arr[i] + arr[j] + arr[k];
 
         if (Math.abs(currentSum - target) < Math.abs(closestSum - target)) {
           closestSum = currentSum;
